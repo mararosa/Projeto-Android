c0105db407bda5e4cbac7d1cf013f3ac87614944
Mara Rosa <edimara.rosa@hotmail.com>
2019-12-20 14:55:00 -0300

    add codigo

--- app/src/main/java/br/com/android/projetoimc/WebActivity.kt
+++ app/src/main/java/br/com/android/projetoimc/WebActivity.kt
@@ -2,11 +2,14 @@ package br.com.android.projetoimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_web.*
 
 class WebActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+        wbvSite.settings.javaScriptEnabled = true
+        wbvSite.loadUrl("http://www.abeso.org.br/atitude-saudavel/imc")
     }
 }
