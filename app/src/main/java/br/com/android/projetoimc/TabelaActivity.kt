a7478f5b53f408c704cbd0b8e2c058884183dcd3
Mara Rosa <edimara.rosa@hotmail.com>
2019-12-20 14:49:53 -0300

    add xml e codigo tela webview

--- app/src/main/java/br/com/android/projetoimc/TabelaActivity.kt
+++ app/src/main/java/br/com/android/projetoimc/TabelaActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_tabela.*
 
 class TabelaActivity : AppCompatActivity() {
 
