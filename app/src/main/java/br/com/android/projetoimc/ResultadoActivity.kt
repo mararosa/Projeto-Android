a32bd4c1d318d7c111d4763cadfec875ceabc2ec
Mara Rosa <edimara.rosa@hotmail.com>
2019-12-20 14:41:25 -0300

    add xml e codigo tela tabela

--- app/src/main/java/br/com/android/projetoimc/ResultadoActivity.kt
+++ app/src/main/java/br/com/android/projetoimc/ResultadoActivity.kt
@@ -8,5 +8,8 @@ class ResultadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+
+
     }
 }
--- app/src/main/java/br/com/android/projetoimc/TabelaActivity.kt
+++ app/src/main/java/br/com/android/projetoimc/TabelaActivity.kt
@@ -1,12 +1,21 @@
 package br.com.android.projetoimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class TabelaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tabela)
+
+        btnSite.setOnClickListener {
+
+            startActivity(Intent(this@TabelaActivity, WebActivity::class.java))
+
+        }
+
     }
 }
